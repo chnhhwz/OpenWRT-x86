efa83b3a9be951228dda76cd5d18dce896c8bc01
chnhhwz <103352599+chnhhwz@users.noreply.github.com>
2023-07-29 13:37:58 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -16,7 +16,7 @@ sed -i 's/192.168.1.1/192.168.100.10/g' package/base-files/files/bin/config_gene
 ########### 设置空密码及版本标识 ###########
 ZZZ="package/lean/default-settings/files/zzz-default-settings"
 sed -i '/CYXluq4wUazHjmCDBCqXF/d' $ZZZ
-sed -i "s/OpenWrt /W.Z $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt /g" $ZZZ
+sed -i "s/OpenWrt /w.z $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt /g" $ZZZ
 
 ########### 更改源码版本 ###########
 # sed -i "s/KERNEL_PATCHVER:=.*/KERNEL_PATCHVER:=6.1/g" target/linux/x86/Makefile
